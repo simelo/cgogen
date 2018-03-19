4342389a9d84f54fd219b586d338b3c001650eb9
Olemis Lang <olemis+work@gmail.com>
2018-03-19 14:18:33 -0400

    [cgogen] refs - Args rules for intf, func, chan, and ellispse types

--- src/cmd/cgogen.go
+++ src/cmd/cgogen.go
@@ -6,28 +6,38 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"log"
 	"os"
 
 	"github.com/dave/jennifer/jen"
 )
 
 type Config struct {
-	Path string
+	Path    string
+	Verbose bool
 }
 
 func (c *Config) register() {
 	flag.StringVar(&c.Path, "i", "", "PATH to source file")
+	flag.BoolVar(&c.Verbose, "v", false, "Print debug message to stdout")
 }
 
 var (
-	cfg Config
+	cfg    Config
+	applog = func(format string, v ...interface{}) {
+		// Logging disabled
+	}
 )
 
 func main() {
 	cfg.register()
 	flag.Parse()
 
-	fmt.Printf("Opening %v \n", cfg.Path)
+	if cfg.Verbose {
+		applog = log.Printf
+	}
+
+	applog("Opening %v \n", cfg.Path)
 	fo, err := os.Open(cfg.Path)
 	if err != nil {
 		fmt.Println(err)
@@ -45,7 +55,7 @@ func main() {
 
 	outFile := jen.NewFile("main")
 	outFile.CgoPreamble(`
-	#include <string.h>
+  #include <string.h>
   #include <stdlib.h>
   
   #include "../../include/skytypes.h"`)
@@ -62,6 +72,7 @@ func main() {
 	}
 
 	fmt.Printf("%#v", outFile)
+	applog("Finished %v", cfg.Path)
 }
 
 func isAsciiUpper(c rune) bool {
@@ -75,13 +86,52 @@ func typeSpecStr(_typeExpr *ast.Expr) string {
 		if arrayExpr, isArray := (*_typeExpr).(*ast.ArrayType); isArray {
 			spec += "[]"
 			_typeExpr = &arrayExpr.Elt
+			continue
 		}
 		if starExpr, isStar := (*_typeExpr).(*ast.StarExpr); isStar {
 			spec += "*"
 			_typeExpr = &starExpr.X
+			continue
 		}
-		if identExpr, isIdent := (*_typeExpr).(*ast.Ident); isIdent {
-			typeName := identExpr.Name
+		if _, isEllipse := (*_typeExpr).(*ast.Ellipsis); isEllipse {
+			spec += "..."
+			_typeExpr = nil
+			continue
+		}
+		if _, isFunc := (*_typeExpr).(*ast.FuncType); isFunc {
+			// TODO: Improve func type translation
+			spec += "C.Handle"
+			_typeExpr = nil
+			continue
+		}
+		if _, isStruct := (*_typeExpr).(*ast.StructType); isStruct {
+			spec += "struct{}"
+			_typeExpr = nil
+			continue
+		}
+		if _, isIntf := (*_typeExpr).(*ast.InterfaceType); isIntf {
+			spec += "interface{}"
+			_typeExpr = nil
+			continue
+		}
+		if _, isChan := (*_typeExpr).(*ast.ChanType); isChan {
+			// TODO: Improve func type translation
+			spec += "C.GoChan_"
+			_typeExpr = nil
+			continue
+		}
+		if mapExpr, isMap := (*_typeExpr).(*ast.MapType); isMap {
+			return spec + "map[" + typeSpecStr(&mapExpr.Key) + "]" + typeSpecStr(&mapExpr.Value)
+		}
+		identExpr, isIdent := (*_typeExpr).(*ast.Ident)
+		selExpr, isSelector := (*_typeExpr).(*ast.SelectorExpr)
+		if isIdent || isSelector {
+			typeName := ""
+			if isIdent {
+				typeName = identExpr.Name
+			} else {
+				typeName = selExpr.Sel.Name
+			}
 			isExported := isAsciiUpper(rune(typeName[0]))
 			if spec == "" && !addPointer && isExported {
 				addPointer = true
@@ -90,6 +140,9 @@ func typeSpecStr(_typeExpr *ast.Expr) string {
 				spec += "C."
 			}
 			spec += typeName
+			_typeExpr = nil
+		} else {
+			applog("No rules to follow with %s", (*_typeExpr).(*ast.Ident))
 			_typeExpr = nil
 		}
 	}
@@ -104,11 +157,14 @@ func argName(name string) string {
 }
 
 func processFunc(fast *ast.File, fdecl *ast.FuncDecl, outFile *jen.File) {
+	funcName := fdecl.Name.Name
+
 	if !fdecl.Name.IsExported() {
+		applog("Skipping %v \n", funcName)
 		return
 	}
 
-	funcName := fdecl.Name.Name
+	applog("Processing %v \n", funcName)
 	var params jen.Statement
 	if receiver := fdecl.Recv; receiver != nil {
 		// Method
@@ -127,7 +183,8 @@ func processFunc(fast *ast.File, fdecl *ast.FuncDecl, outFile *jen.File) {
 	}
 
 	cfuncName := "SKY_" + fast.Name.Name + "_" + funcName
-	stmt := outFile.Func().Comment("export " + cfuncName).Id(cfuncName)
+	stmt := outFile.Comment("export " + cfuncName)
+	stmt = outFile.Func().Id(cfuncName)
 
 	allparams := fdecl.Type.Params.List[:]
 	var retField *ast.Field = nil
@@ -135,7 +192,10 @@ func processFunc(fast *ast.File, fdecl *ast.FuncDecl, outFile *jen.File) {
 		lastFieldIdx := len(fdecl.Type.Results.List) - 1
 		retField = fdecl.Type.Results.List[lastFieldIdx]
 		_, isArray := retField.Type.(*ast.ArrayType)
-		if isArray || retField.Type.(*ast.Ident).IsExported() {
+		_, isQual := retField.Type.(*ast.SelectorExpr)
+		_, isIdent := retField.Type.(*ast.Ident)
+
+		if isArray || isQual || (isIdent && retField.Type.(*ast.Ident).IsExported()) {
 			allparams = append(allparams, fdecl.Type.Results.List[:]...)
 			retField = nil
 		} else {
@@ -187,7 +247,7 @@ func processFunc(fast *ast.File, fdecl *ast.FuncDecl, outFile *jen.File) {
 	}
 
 	if retField != nil {
-		retName := retField.Type.(*ast.Ident).Name
+		retName := typeSpecStr(&retField.Type)
 		if retName == "error" {
 			stmt = stmt.Id("C.uint")
 		} else {
